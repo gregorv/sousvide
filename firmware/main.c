21f7d5733196017a5a72ef590b7be44c49405d70
Gregor Vollmer <gregor@celement.de>
2016-12-28 11:50:15 +0100

    Main Program: Removed old code and included new modules
    Use control and com module and glue it together with onewire
    temperature readout.

--- firmware/main.c
+++ firmware/main.c
@@ -1,177 +1,82 @@
 #include "main.h"
 
+fix16_t g_current_temperature;
 
 
+int tempsens_start_measure(void){
+	if( W1_IN & 1<< W1_PIN ){
+		w1_command( CONVERT_T, NULL );
+		W1_OUT |= 1<< W1_PIN;
+		W1_DDR |= 1<< W1_PIN; // parasite power on
+	}else{
+		uart_puts_P("Short Circuit!\r\n");
+		return 0;
 	}
+	return 1;
 }
 
-uint16_t temperatureReadStatus()
+
+fix16_t tempsens_get_temperature()
 {
-	TEMPERATURE_CS_PORT &= ~(1<<TEMPERATURE_CS);
-	SPDR = 0;
-	while((SPSR & (1<<SPIF)) == 0);
-	uint8_t h = SPDR;
-	SPDR = 0;
-	while((SPSR & (1<<SPIF)) == 0);
-	uint8_t l = SPDR;
-	TEMPERATURE_CS_PORT |= (1<<TEMPERATURE_CS);
-	return (((uint16_t)h) << 8) + l;
-}
+	unsigned char id[8], diff;
+	for(diff = SEARCH_FIRST; diff != LAST_DEVICE;) {
+		diff = w1_rom_search(diff, id);
 
-uint16_t temperatureAverage()
-{
-	uint16_t temp = TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	temp += TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	temp += TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	temp += TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	temp += TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	temp += TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	temp += TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	temp += TS_TEMPERATURE(temperatureReadStatus());
-	_delay_ms(300);
-	return temp >> 3;
-}
-
-void start_meas( void ){
-  if( W1_IN & 1<< W1_PIN ){
-    w1_command( CONVERT_T, NULL );
-    W1_OUT |= 1<< W1_PIN;
-    W1_DDR |= 1<< W1_PIN;			// parasite power on
-
-  }else{
-	  control_send_str("Short Circuit!\r\n");
-  }
-}
-
-
-void read_meas( void )
-{
-  uchar id[8], diff;
-  uchar s[30];
-  uchar i;
-  uint temp;
-
-  for( diff = SEARCH_FIRST; diff != LAST_DEVICE; ){
-    diff = w1_rom_search( diff, id );
-
-    if( diff == PRESENCE_ERR ){
-      control_send_str( "No Sensor found" );
-      break;
-    }
-    if( diff == DATA_ERR ){
-      control_send_str( "Bus Error" );
-      break;
-    }
-    if( id[0] == 0x28 || id[0] == 0x10 ){	// temperature sensor
-      control_send_str( "ID: " );
-      for( i = 0; i < 8; i++ ){
-	sprintf( s, "%02X ", id[i] );
-	control_send_str( s );
-      }
-      w1_byte_wr( READ );			// read command
-      temp = w1_byte_rd();			// low byte
-      temp |= (uint)w1_byte_rd() << 8;		// high byte
-      if( id[0] == 0x10 )			// 9 -> 12 bit
-        temp <<= 3;
-      sprintf( s, "  T: %04X = ", temp );	// hex value
-      control_send_str( s );
-      sprintf( s, "%4d.%01døC", temp >> 4, (temp << 12) / 6553 ); // 0.1øC
-      control_send_str( s );
-    }
-  }
-  control_send_str( "\r\n" );
+		if(diff == PRESENCE_ERR) {
+			uart_puts_P("\aNo Sensor found\r\n");
+			break;
+		}
+		if(diff == DATA_ERR) {
+			uart_puts_P("\aBus Error\r\n");
+			break;
+		}
+		if(id[0] == 0x28 || id[0] == 0x10){	// temperature sensor
+			w1_byte_wr(READ);			// read command
+			unsigned int temp = w1_byte_rd();	// low byte
+			temp |= (uint)w1_byte_rd() << 8;	// high byte
+			if( id[0] == 0x10 )			// 9 -> 12 bit
+				temp <<= 3;
+			fix16_t fixtemp = fix16_from_int(temp) >> 4; // 0.5°C / 8 -> 1/16°C
+			return fixtemp;
+		}
+	}
+	uart_puts("\aNo Temperature Sensor\r\n");
+	return fix16_min;
 }
 
 
 int main()
 {
 	init_timer();
-	control_init();
+	uart_init(UART_BAUD_SELECT(F_CPU, BAUD));
+	controller_init();
+	com_init();
+	set_sleep_mode(SLEEP_MODE_IDLE);
 	sei();
-	control_send_str("Hello.\r\n");
+	uint8_t first = 3;
 	for(;;) {
-		start_meas();
-		_delay_ms(500);
-		read_meas();
-		_delay_ms(500);
+		if(second == 1) {
+			tempsens_start_measure();
+		} else if(second == 2) {
+			g_current_temperature = tempsens_get_temperature();
+			if(g_current_temperature == fix16_min) {
+				// EMERGENCY SHUTDOWN!!
+				controller_disable_dimmer();
+			} else {
+				if(first) {
+					controller_reset(g_current_temperature);
+					--first;
+				} else {
+					controller_step_input_temperature(g_current_temperature);				}
+			}
+			second = 0;
+		}
+		com_handle();
+		sleep_enable();
+		sei();
+		sleep_cpu();
+		sleep_disable();
 	}
-//	wifi_connect();
-/*
-	float target_temperature = 60.0f;
-	const float temperature_calibration = -2.0f;
-	const float p = 5.0f;
-	const float i = 0.01f;
-	float i_cur = 0.0f;
-	uint8_t wind_up = FALSE;
-	const float dt = 1.2f;
-	while(1)
-	{
-//		wifi_connect();
-		float temperature = temperatureAverage()/4.0f + temperature_calibration;
-		float error = target_temperature - temperature;
-		if(!wind_up) {
-			i_cur += dt*(error);
-		}
-		float power = p * error + i * i_cur;
-		if(power > 255.0f) {
-			g_dimmerDuty = 255;
-			wind_up = TRUE;
-		}
-		else if(power < 0.0f) {
-			wind_up = TRUE;
-			g_dimmerDuty = 0;
-		} else {
-			wind_up = FALSE;
-			g_dimmerDuty = power;
-		}
-		char str[50];
-		sprintf(str, "TEiDw %.2f %.2f %.5f %i %i\r\n", temperature, error, i_cur, g_dimmerDuty, wind_up); 
-		control_send_str(str);
-	}*/
 	return 0;
-}
-
-ISR(USART_RXC_vect)
-{
-	control_receive_byte(UDR);
-}
-
-void control_init()
-{
-    DDRD |= (1<<PD1);
-    UCSRB |= (1<<RXCIE); // enable TX interupt
-    UCSRB |= (1<<TXEN) | (1<<RXEN);
-    UCSRC |= (1<<USBS) | (1<<UCSZ1) | (1<<UCSZ0); // 8 bit, 2 stop, async
-	UBRRH = UBRRH_VALUE;
-	UBRRL = UBRRL_VALUE;
-#if USE_2X
-	UCSRA |= (1 << U2X);
-#else
-	UCSRA &= ~(1 << U2X);
-#endif
-}
-
-void control_send_str(const char* str)
-{
-	for(; *str != '\0'; str++) {
-		control_send_byte(*str);
-	}
-}
-
-void control_send_byte(char b)
-{
-    while(!(UCSRA & (1<<UDRE)));
-    UDR = b;
-}
-
-void control_receive_byte(char b)
-{
 }
 
